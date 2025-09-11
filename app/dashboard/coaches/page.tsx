591e67bb6f8141e1c561c5a9440f80ced21d5699
younes <jonaselemented@email.com>
2025-09-11 13:51:29 -0700

    update

--- app/dashboard/coaches/page.tsx
+++ app/dashboard/coaches/page.tsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import {
+  EllipsisVerticalIcon,
+  LocationEditIcon,
+  MapPinIcon,
+  MessageCircleMoreIcon,
+} from "lucide-react";
+import Image from "next/image";
+import React from "react";
 
 const CoachesPage = () => {
   return (
-    <div>CoachesPage</div>
-  )
-}
+    <>
+      <div className="f-align justify-between mb-6">
+        <span className="space-y-1.5">
+          <p className="font-medium">Find the best coaches online</p>
+          <p className="text-sm text-black/30">
+            You can find coaches , talk to them online via our chatbox on our
+            platform
+          </p>
+        </span>
 
-export default CoachesPage
+        <div className="f-align gap-3 hidden sm:flex">
+          <div className="f-align gap-1.5 justify-center lg:justify-start flex-shrink-0">
+            <div className="size-10 lg:size-10 rounded-full relative">
+              <Image
+                src="/images/avatar2.jpg"
+                alt="avatar"
+                fill
+                className="absolute object-cover rounded-full"
+              />
+            </div>
+            <span className="flex-col hidden lg:flex space-y-1.5">
+              <p className="font-medium text-xs">Jasmine</p>
+              <p className="text-black/30 text-xs">Prodigy</p>
+            </span>
+          </div>
+
+          <EllipsisVerticalIcon size={20} />
+        </div>
+      </div>
+
+      <div className="mt-6 grid grid-cols-1 gap-3 lg:grid-cols-3">
+        {Array.from({ length: 3 }).map((_, i) => (
+          <div
+            key={i}
+            className="card flex gap-1.5 bg-white rounded-3xl min-h-[160px] p-1"
+          >
+            <div className="bg-foreground flex-3 space-y-3 flex flex-col items-center justify-center rounded-2xl">
+              <div className="rounded-full w-20 h-20 relative">
+                <Image
+                  alt="coach"
+                  src="/images/coach1.jpg"
+                  className="absolute rounded-full object-contain"
+                  fill
+                />
+              </div>
+              <div className="flex items-center gap-1.5">
+                <button className="flex items-center justify-center gap-1.5 bg-secondary rounded-lg p-1.5 text-sm text-white">
+                  <span>chat</span>
+                  <MessageCircleMoreIcon size={17} />
+                </button>
+              </div>
+            </div>
+            <div className="flex-4 p-1.5 rounded-2xl flex flex-col justify-between h-full items-start">
+              <p className="bg-light py-1.5 px-3 rounded-lg text-sm inline-block">
+                Product designer
+              </p>
+              <p className="flex items-center gap-1 ">
+                <span className="font-medium text-sm">Nova Shierly</span>
+                <Image
+                  alt="verified"
+                  src="/images/icons/verified.svg"
+                  width={15}
+                  height={15}
+                />
+              </p>
+              <p className="flex gap-3 inline-flex">
+                <span className="text-sm">$170.00/hr</span>
+                <span className="flex gap-1 items-center">
+                  <MapPinIcon size={17} />
+                  <span className="text-sm">Phoenix, AZ</span>
+                </span>
+              </p>
+              <ul className="flex gap-3 text-sm">
+                <li className="bg-foreground inline-block rounded-lg px-1.5 py-1">
+                  UI/UX
+                </li>
+                <li className="bg-foreground inline-block rounded-lg px-1.5 py-1">
+                  Motion
+                </li>
+                <li className="bg-foreground inline-block rounded-lg px-1.5 py-1">
+                  Illustrator
+                </li>
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default CoachesPage;
--- app/dashboard/study/page.tsx
+++ app/dashboard/study/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import SubjectsBreakdownChart from "@/features/study/organisms/SubjectsBreakdownChart";
 import OverviewCard from "@/shared/navigation/components/OverviewCard";
 import {
   ArrowUpFromDotIcon,
+  BookPlusIcon,
   BookUp2Icon,
+  CheckIcon,
   ClockArrowUpIcon,
+  DownloadIcon,
   EllipsisVerticalIcon,
+  FileTextIcon,
+  HistoryIcon,
+  TrashIcon,
 } from "lucide-react";
 import Image from "next/image";
 import React, { useState } from "react";
@@ -66,6 +73,133 @@ const StudyPage = () => {
           unit="Days"
           icon={<ArrowUpFromDotIcon size={40} />}
         />
+      </div>
+
+      <div className="my-6 grid grid-cols-1 lg:grid-cols-8 gap-6 items-stretch min-h-[300px]">
+        <div className="col-span-3 flex flex-col">
+          <h2 className="text-2xl font-medium mb-6">Sessions Today</h2>
+
+          <div className="sessions-div space-y-3 rounded-3xl justify-between  w-full h-auto">
+            <div className="logs space-y-3">
+              <div className="f-align justify-between rounded-2xl p-3 bg-white">
+                <p className="">Math</p>
+                <p>45 min</p>
+                <p>😊</p>
+                <div className="f-align gap-1.5">
+                  <button className="bg-red-500 text-white p-1 rounded-lg">
+                    <TrashIcon size={20} />
+                  </button>
+                  <button className="f-align gap-1.5 *:text-white bg-progress rounded-lg p-1">
+                    <CheckIcon size={20} />
+                  </button>
+                </div>
+              </div>
+                            <div className="f-align justify-between rounded-2xl p-3 bg-white">
+                <p className="">Math</p>
+                <p>45 min</p>
+                <p>😊</p>
+                <div className="f-align gap-1.5">
+                  <button className="bg-red-500 text-white p-1 rounded-lg">
+                    <TrashIcon size={20} />
+                  </button>
+                  <button className="f-align gap-1.5 *:text-white bg-progress rounded-lg p-1">
+                    <CheckIcon size={20} />
+                  </button>
+                </div>
+              </div>
+                            <div className="f-align justify-between rounded-2xl p-3 bg-white">
+                <p className="">Math</p>
+                <p>45 min</p>
+                <p>😊</p>
+                <div className="f-align gap-1.5">
+                  <button className="bg-red-500 text-white p-1 rounded-lg">
+                    <TrashIcon size={20} />
+                  </button>
+                  <button className="f-align gap-1.5 *:text-white bg-progress rounded-lg p-1">
+                    <CheckIcon size={20} />
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full f-align  justify-between items-end">
+              <button className="f-align gap-1.5 bg-accent rounded-lg p-1.5 text-sm *:text-white">
+                <span>Add session</span>
+                <BookPlusIcon size={20} />
+              </button>
+              <button className="f-align gap-1.5 bg-secondary rounded-lg p-1.5 text-sm *:text-white">
+                <span>History logs</span>
+                <HistoryIcon size={20} />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-2 flex flex-col">
+          <h2 className="text-2xl font-medium mb-6">Subjects Breakdown</h2>
+          <div className="bg-white p-6 rounded-4xl flex-1 w-full">
+            <SubjectsBreakdownChart />
+          </div>
+        </div>
+
+        <div className="col-span-3 flex flex-col">
+          <h2 className="text-2xl font-medium mb-6">Book Suggestion</h2>
+          <div className="bg-white rounded-4xl gap-3 grid grid-cols-5 flex-1 w-full p-3">
+            <div
+              style={{ background: "url('/images/book.jpg')" }}
+              className="relative f-center col-span-2 rounded-3xl bg-secondary"
+            >
+              <div
+                className="absolute inset-0 bg-cover bg-center filter blur-xs rounded-3xl opacity-60"
+                style={{ backgroundImage: "url('/images/book.jpg')" }}
+              ></div>
+              <Image
+                className="z-10 rounded-2xl"
+                src={"/images/book.jpg"}
+                alt="book"
+                width={100}
+                height={100}
+              />
+            </div>
+
+            <div className="col-span-3">
+              <div className="content text-sm font-medium flex flex-col h-full justify-between py-3">
+                <div className="space-y-1.5">
+                  <p className="text-xl">Follow me to ground</p>
+                  <p>
+                    <span className="text-black/40 font-light">Author : </span>
+                    <span className="">Sue rainsford</span>
+                  </p>
+                  <p>
+                    <span className="text-black/40 font-light">Genre : </span>
+                    <span>Mysterious</span>
+                  </p>
+                  <p>
+                    <span className="text-black/40 font-light">Pages : </span>
+                    <span>309</span>
+                  </p>
+                  <p>
+                    <span className="text-black/40 font-light">
+                      Published year :{" "}
+                    </span>
+                    <span>2013</span>
+                  </p>
+                </div>
+
+                <div className="f-align gap-3">
+                  <button className="text-white f-align gap-1.5 bg-secondary p-1.5 rounded-lg">
+                    <span>Download</span>
+                    <DownloadIcon size={20}/>
+                  </button>
+                   <button className="text-white f-align gap-1.5 bg-secondary p-1.5 rounded-lg">
+                    <span>Summary</span>
+                    <FileTextIcon size={20}/>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
