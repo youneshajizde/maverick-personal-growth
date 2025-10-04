d60a1c2e16cd6c6dcad3ea88163bedf3010c383c
youneshajizde <jonaselemented@gmail.com>
2025-10-04 20:17:02 +0330

    multiselect is kinda finished

--- shared/components/molecules/MultiSelect.tsx
+++ shared/components/molecules/MultiSelect.tsx
@@ -3,28 +3,60 @@
 import { OptionT } from "@/shared/constants/shared.constants";
 import { cn } from "@/shared/utils/functions";
 import { ChevronDownIcon } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface MultiSelectProps {
   label?: string;
-  options?: OptionT[];
+  options: OptionT[];
   className?: string;
 }
 
 const MultiSelect = ({ label, options, className }: MultiSelectProps) => {
   const [open, setOpen] = useState(false);
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+  const selectRef = useRef<HTMLDivElement>(null);
+  console.log("this is multiselect bitch : ", selectedItems);
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        selectRef.current &&
+        !selectRef.current.contains(event.target as Node)
+      ) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const multiSelectHandler = (item: string) => {
+    setSelectedItems((prev) => {
+      if (prev.includes(item)) {
+        return prev
+      }
+      return [...prev, item];
+    });
+
+    if(selectedItems.includes(item)) {
+        setSelectedItems((prev) => selectedItems.filter((si) => si !== item))
+    }
+  };
 
   const openHandler = () => {
     setOpen((prev) => !prev);
   };
   return (
     <>
-      {label && <label className="font medium text-sm mb-1.5">{label}</label>}
+      {label && <label className="font-medium text-sm">{label}</label>}
 
-      <div className={cn("relative w-full", className)}>
+      <div ref={selectRef} className={cn("relative w-full", className)}>
         <button
           onClick={openHandler}
-          className="btn btn-white f-align justify-between gap-3 w-full transition-all duration-200"
+          className={`btn btn-white f-align justify-between gap-3 w-full transition-all duration-200 ${
+            label && "mt-1.5"
+          }`}
         >
           <span>something</span>
           <ChevronDownIcon
@@ -37,12 +69,14 @@ const MultiSelect = ({ label, options, className }: MultiSelectProps) => {
 
         <ul
           className={`absolute top-full mt-1 w-full bg-white rounded-xl border border-gray-200 shadow-lg p-3 z-30 max-h-60 overflow-auto space-y-1 transition-all duration-200 ease-in-out ${
-            open ? "opacity-100 -translate-y-2" : "opacity-0 translate-y-0"
+            open ? "opacity-100 -translate-y-0" : "opacity-0 translate-y-2"
           }`}
         >
           {options?.map((opt, i) => (
             <li
-              className="cursor-pointer px-3 py-2 truncate text-sm text-gray-700 rounded-lg transition-colors duration-150 hover:bg-gray-100"
+
+              onClick={() => multiSelectHandler(opt.value)}
+              className={`cursor-pointer px-3 py-2 truncate text-sm text-gray-700 rounded-lg transition-colors duration-150 hover:bg-gray-100 ${selectedItems.includes(opt.value) && "bg-secondary text-white hover:bg-secondary"}`}
               key={i}
             >
               {opt.value}
